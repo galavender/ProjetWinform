866b6e1dd575f66294ffe209873be2f7dfa6d1ec
glavender <glavender@GLAVENDER17-DE>
2017-05-05 13:42:54 +0200

    maj

---
+++ JobOverview/Création des tables.sql
@@ -0,0 +1,34 @@
+﻿if exists (select * from sys.types where name='TypeTableTache')
+drop type TypeTableTache
+go
+
+create type TypeTableTache 
+as table
+(
+	IdTache uniqueidentifier,
+	Libelle nvarchar(40),
+	Annexe bit,
+	CodeActivite varchar(20),
+	Login varchar(20),
+	description nvarchar(1000) not null
+)
+go
+
+
+if exists (select * from sys.types where name='TypeTableTacheProd')
+drop type TypeTableTacheProd
+go
+
+
+create type TypeTableTacheProd
+as table
+(
+	IdTache uniqueidentifier,
+	DureePrevue float(5),
+	DureeRestanteEstimee float(5),
+	CodeModule varchar(20),
+	CodeLogicielModule varchar(20),
+	NumeroVersion float(4),
+	CodeLogicielVersion varchar(20)
+)
+go
